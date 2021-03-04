50acebddfd4e09619069cf6918f53c1c5f6eb6d8
Irish <76734569+hiroki0812@users.noreply.github.com>
2021-03-05 00:09:50 +0900

    テキスト2まで済

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,37 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了</p>
+        <ul>
+          <div className="list-row">
+            <li>ああああ</li>
+            <button>追加</button>
+            <button>削除</button>
+          </div>
+          <div className="list-row">
+            <li>ああああ</li>
+            <button>追加</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了</p>
+        <ul>
+          <div className="list-row">
+            <li>ああああ</li>
+            <button>追加</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
